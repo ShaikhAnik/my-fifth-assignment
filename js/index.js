9f9827c1a3037ec286122b262e42c956d439ad33
shaikh anik <aniks665@gmail.com>
2024-09-23 07:47:03 +0600

    js code added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,77 @@
-console.log(5);
+
+ document.getElementById('btn-add-donate').addEventListener('click',function(){
+        const addDonation = getInputFieldValueById('input-add-donation');
+        const donateBalance =getTextFieldValueById('donation-balance');
+        if( typeof addDonation === 'number' && !isNaN(addDonation) && addDonation >0){
+        
+
+           
+    
+            const newDonationBalance= addDonation + donateBalance;
+            document.getElementById('donation-balance').innerText= newDonationBalance;
+             
+        const totalBalance= document.getElementById('total-balance')
+        const totalBalanceFinal= parseFloat(totalBalance.innerText);
+        const newTotalBalance = totalBalanceFinal - addDonation;
+        totalBalance.innerText = newTotalBalance;
+        
+        
+        
+        }else{
+            alert("Invalid Donation Amount");
+        }
+
+ })
+ 
+
+ document.getElementById('btn-add-donate2').addEventListener('click',function(){
+    const addDonationSecond = getInputFieldValueById('input-add-donation2');
+    const donateBalanceSecond =getTextFieldValueById('donation-balance2');
+    if( typeof addDonationSecond === 'number' && !isNaN(addDonationSecond) && addDonationSecond >0){
+    
+
+       
+
+        const newDonationBalance2= addDonationSecond + donateBalanceSecond;
+        document.getElementById('donation-balance2').innerText= newDonationBalance2;
+         
+                
+        const totalBalance= getTextFieldValueById('total-balance')
+        
+        const newTotalBalance = totalBalance - addDonationSecond;
+        document.getElementById('total-balance').innerText = newTotalBalance;
+    
+    
+    }else{
+        alert("Invalid Donation Amount");
+    }
+
+})
+
+document.getElementById('btn-add-donate3').addEventListener('click',function(){
+    const addDonationThird = getInputFieldValueById('input-add-donation3');
+    const donateBalanceThird =getTextFieldValueById('donation-balance3');
+    if( typeof addDonationThird === 'number' && !isNaN(addDonationThird) && addDonationThird >0){
+    
+
+       
+
+        const newDonationBalance3= addDonationThird + donateBalanceThird;
+        document.getElementById('donation-balance3').innerText= newDonationBalance3;
+         
+                
+        const totalBalance= getTextFieldValueById('total-balance')
+        
+        const newTotalBalance = totalBalance - addDonationThird;
+        document.getElementById('total-balance').innerText = newTotalBalance;
+    
+    
+    }else{
+        alert("Invalid Donation Amount");
+    }
+
+})
+ 
+ 
+ 
+ 
