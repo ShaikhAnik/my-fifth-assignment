4b5bab61a671bb99a3bc7a7afed3df15184e2007
shaikh anik <aniks665@gmail.com>
2024-09-23 22:45:00 +0600

    javascript more function added

--- js/index.js
+++ js/index.js
@@ -15,7 +15,16 @@
         const newTotalBalance = totalBalanceFinal - addDonation;
         totalBalance.innerText = newTotalBalance;
         
+        const div = document.createElement('div');
+        div.classList.add('border','border-solid','rounded','border-gray-500','p-5','mb-6','mt-7');
+        div.innerHTML =`
+               <p class="text-lg font-bold">${addDonation} Taka is Donated for Flood at Noakhali, Bangladesh</p>
+            <p class="text-gray-500"> Date : ${new Date().toString()}  </p>
+        `
+        document.getElementById('history-donation').appendChild(div);
+
        
+
         
         }else{
             alert("Invalid Donation Amount");
@@ -41,6 +50,16 @@
         const newTotalBalance = totalBalance - addDonationSecond;
         document.getElementById('total-balance').innerText = newTotalBalance;
     
+
+
+        const div = document.createElement('div');
+        div.classList.add('border','border-solid','rounded','border-gray-500','p-5','mb-6','mt-7');
+        div.innerHTML =`
+               <p class="text-lg font-bold">${addDonationSecond} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+            <p class="text-gray-500"> Date : ${new Date().toString()}  </p>
+        `
+        document.getElementById('history-donation').appendChild(div);
+
     
     }else{
         alert("Invalid Donation Amount");
@@ -64,7 +83,16 @@ document.getElementById('btn-add-donate3').addEventListener('click',function(){
         
         const newTotalBalance = totalBalance - addDonationThird;
         document.getElementById('total-balance').innerText = newTotalBalance;
-    
+
+        
+        const div = document.createElement('div');
+        div.classList.add('border','border-solid','rounded','border-gray-500','p-5','mb-6','mt-7');
+        div.innerHTML =`
+               <p class="text-lg font-bold">${addDonationThird} Taka is Donated for Aid for Injured in the Quota Movement</p>
+            <p class="text-gray-500"> Date : ${new Date().toString()}  </p>
+        `
+        document.getElementById('history-donation').appendChild(div);   
+        
     
     }else{
         alert("Invalid Donation Amount");
@@ -73,6 +101,21 @@ document.getElementById('btn-add-donate3').addEventListener('click',function(){
 })
  
  
+const historyBtn =document.getElementById('history-button');
+const donationBtn =document.getElementById('donation-btn');
+historyBtn.addEventListener('click',function(){
+    historyBtn.classList.add('bg-secondary', "text-primary");
 
+   
+  donationBtn.classList.remove('bg-secondary', "text-primary");
 
+  document.getElementById('card-section').classList.add('hidden');
+})
+
+donationBtn.addEventListener('click',function(){
+    donationBtn.classList.add('bg-secondary', "text-primary");
+    historyBtn.classList.remove('bg-secondary', "text-primary");
+document.getElementById('card-section').classList.remove('hidden');
+
+})
  
