f9069db43b2d4db35af86ad132a5f9d971fb280f
shaikh anik <aniks665@gmail.com>
2024-09-24 06:14:17 +0600

    blog and js code added

--- js/index.js
+++ js/index.js
@@ -2,29 +2,31 @@
  document.getElementById('btn-add-donate').addEventListener('click',function(){
         const addDonation = getInputFieldValueById('input-add-donation');
         const donateBalance =getTextFieldValueById('donation-balance');
-        if( typeof addDonation === 'number' && !isNaN(addDonation) && addDonation >0){
+        const totalBalance= getTextFieldValueById('total-balance');
+        if( typeof addDonation === 'number' && !isNaN(addDonation) && addDonation >0 && addDonation <= totalBalance){
         
-
-           
-    
-            const newDonationBalance= addDonation + donateBalance;
-            document.getElementById('donation-balance').innerText= newDonationBalance;
+         
+        
+           const newDonationBalance= addDonation + donateBalance;
+           document.getElementById('donation-balance').innerText= newDonationBalance;
              
-        const totalBalance= document.getElementById('total-balance')
-        const totalBalanceFinal= parseFloat(totalBalance.innerText);
-        const newTotalBalance = totalBalanceFinal - addDonation;
-        totalBalance.innerText = newTotalBalance;
-        
-        const div = document.createElement('div');
-        div.classList.add('border','border-solid','rounded','border-gray-500','p-5','mb-6','mt-7');
-        div.innerHTML =`
-               <p class="text-lg font-bold">${addDonation} Taka is Donated for Flood at Noakhali, Bangladesh</p>
-            <p class="text-gray-500"> Date : ${new Date().toString()}  </p>
-        `
-        document.getElementById('history-donation').appendChild(div);
+           
+            
+            const newTotalBalance = totalBalance - addDonation;
+            
+            document.getElementById('total-balance').innerText= newTotalBalance;
 
-       
 
+                
+            const div = document.createElement('div');
+            div.classList.add('border','border-solid','rounded','border-gray-500','p-5','mb-6','mt-7');
+            div.innerHTML =`
+                <p class="text-lg font-bold">${addDonation} Taka is Donated for Flood at Noakhali, Bangladesh</p>
+                <p class="text-gray-500"> Date : ${new Date().toString()}  </p>
+            `
+            document.getElementById('history-donation').appendChild(div);
+
+     
         
         }else{
             alert("Invalid Donation Amount");
@@ -32,11 +34,14 @@
 
  })
  
+  
+
 
  document.getElementById('btn-add-donate2').addEventListener('click',function(){
     const addDonationSecond = getInputFieldValueById('input-add-donation2');
     const donateBalanceSecond =getTextFieldValueById('donation-balance2');
-    if( typeof addDonationSecond === 'number' && !isNaN(addDonationSecond) && addDonationSecond >0){
+    const totalBalance= getTextFieldValueById('total-balance');
+    if( typeof addDonationSecond === 'number' && !isNaN(addDonationSecond) && addDonationSecond >0 && addDonationSecond <= totalBalance){
     
 
        
@@ -45,11 +50,11 @@
         document.getElementById('donation-balance2').innerText= newDonationBalance2;
          
                 
-        const totalBalance= getTextFieldValueById('total-balance')
+       
         
         const newTotalBalance = totalBalance - addDonationSecond;
         document.getElementById('total-balance').innerText = newTotalBalance;
-    
+
 
 
         const div = document.createElement('div');
@@ -70,7 +75,8 @@
 document.getElementById('btn-add-donate3').addEventListener('click',function(){
     const addDonationThird = getInputFieldValueById('input-add-donation3');
     const donateBalanceThird =getTextFieldValueById('donation-balance3');
-    if( typeof addDonationThird === 'number' && !isNaN(addDonationThird) && addDonationThird >0){
+    const totalBalance= getTextFieldValueById('total-balance');
+    if( typeof addDonationThird === 'number' && !isNaN(addDonationThird) && addDonationThird >0 && addDonationThird <= totalBalance){
     
 
        
@@ -79,16 +85,19 @@ document.getElementById('btn-add-donate3').addEventListener('click',function(){
         document.getElementById('donation-balance3').innerText= newDonationBalance3;
          
                 
-        const totalBalance= getTextFieldValueById('total-balance')
+        
         
         const newTotalBalance = totalBalance - addDonationThird;
         document.getElementById('total-balance').innerText = newTotalBalance;
 
         
+       
+
+
         const div = document.createElement('div');
         div.classList.add('border','border-solid','rounded','border-gray-500','p-5','mb-6','mt-7');
-        div.innerHTML =`
-               <p class="text-lg font-bold">${addDonationThird} Taka is Donated for Aid for Injured in the Quota Movement</p>
+        div.innerHTML = `
+            <p class="text-lg font-bold">${addDonationThird} Taka is Donated for Aid for Injured in the Quota Movement</p>
             <p class="text-gray-500"> Date : ${new Date().toString()}  </p>
         `
         document.getElementById('history-donation').appendChild(div);   
@@ -110,12 +119,19 @@ historyBtn.addEventListener('click',function(){
   donationBtn.classList.remove('bg-secondary', "text-primary");
 
   document.getElementById('card-section').classList.add('hidden');
+  document.getElementById('history-donation').classList.remove('hidden');
 })
 
 donationBtn.addEventListener('click',function(){
     donationBtn.classList.add('bg-secondary', "text-primary");
     historyBtn.classList.remove('bg-secondary', "text-primary");
 document.getElementById('card-section').classList.remove('hidden');
+document.getElementById('history-donation').classList.add('hidden');
 
 })
  
+
+
+
+
+  
